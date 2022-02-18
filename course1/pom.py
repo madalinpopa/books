647c930e33abbbe4877f4b5e045dda65efe98523
Madalin Popa <github@madalintech.com>
2022-02-18 06:20:29 +0200

    save

---
+++ course1/pom.py
@@ -0,0 +1,23 @@
+class TrainingGroundPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.url = "https://techstepacademy.com/training-ground"
+
+    def go(self):
+        self.driver.get(self.url)
+
+    def type_into_input(self, text):
+        inpt = self.driver.find_element_by_id('ipt1')
+        inpt.clear()
+        inpt.send_keys(text)
+        return None
+
+    def get_input_text(self):
+        inpt = self.driver.find_element_by_id('ipt1')
+        elem_text = inpt.get_attribute('value')
+        return elem_text
+
+    def click_button_1(self):
+        button = self.driver.find_element_by_id('b1')
+        button.click()
+        return None
