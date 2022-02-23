a4dae87cf4912330299d88d90f175f4e238d35e0
Madalin Popa <github@madalintech.com>
2022-02-23 06:43:02 +0200

    save

--- book8/mysite/blog/views.py
+++ book8/mysite/blog/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import get_object_or_404, render, get_list_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from .models import Post
+from .forms import EmailPostForm
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by =3
-    template_name = 'blog/post/list.html'
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 
 def post_list(request):
@@ -26,9 +29,6 @@ def post_list(request):
     return render(request, "blog/post/list.html", {"posts": posts, "page": page})
 
 
-
-
-
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
@@ -39,3 +39,23 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
     )
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+def post_share(request, post_id):
+    # retrive post by id
+    post = get_object_or_404(Post, id=post_id, status="published")
+    sent = False
+    if request.method == "POST":
+        # form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form field passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, "admin@myblog.com", [cd['to']])
+            sent = True
+        else:
+            form = EmailPostForm()
+        return render(request, "blog/post/share.html", {"post": post, "form": form, 'sent': sent})
