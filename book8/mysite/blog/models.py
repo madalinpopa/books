da39a96ebc119487203896bec38be08a38291340
Madalin Popa <github@madalintech.com>
2022-02-23 07:32:32 +0200

    save

--- book8/mysite/blog/models.py
+++ book8/mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -29,6 +30,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    tags = TaggableManager()
 
     class Meta:
         ordering = ("-publish",)
@@ -46,3 +48,19 @@ class Post(models.Model):
                 self.slug,
             ],
         )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ("created",)
+
+    def __str__(self) -> str:
+        return f"Comment by {self.name} on {self.post}"
--- book8/mysite/blog/views.py
+++ book8/mysite/blog/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import get_object_or_404, render, get_list_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
+from taggit.models import Tag
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -14,8 +16,12 @@ class PostListView(ListView):
     template_name = "blog/post/list.html"
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get("page")
     try:
@@ -26,7 +32,9 @@ def post_list(request):
     except EmptyPage:
         # if page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, "blog/post/list.html", {"posts": posts, "page": page})
+    return render(
+        request, "blog/post/list.html", {"posts": posts, "page": page, "tag": tag}
+    )
 
 
 def post_detail(request, year, month, day, post):
@@ -38,7 +46,39 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
-    return render(request, "blog/post/detail.html", {"post": post})
+    # list of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == "POST":
+        # a comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # create comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # assign the current post to the comment
+            new_comment.post = post
+            # save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    # list of similar posts
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )
+    return render(
+        request,
+        "blog/post/detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "new_comment": new_comment,
+            "comment_form": comment_form,
+            "similar_posts": similar_posts,
+        },
+    )
 
 
 def post_share(request, post_id):
@@ -54,8 +94,10 @@ def post_share(request, post_id):
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f"{cd['name']} recommends you read {post.title}"
             message = f"Read {post.title} at {post_url}\n\n {cd['name']}'s comments: {cd['comments']}"
-            send_mail(subject, message, "admin@myblog.com", [cd['to']])
+            send_mail(subject, message, "admin@myblog.com", [cd["to"]])
             sent = True
-        else:
-            form = EmailPostForm()
-        return render(request, "blog/post/share.html", {"post": post, "form": form, 'sent': sent})
+    else:
+        form = EmailPostForm()
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
