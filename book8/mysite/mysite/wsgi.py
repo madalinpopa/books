e2bdcde15bf954fc8ebf8545d600269f88251512
Madalin Popa <github@madalintech.com>
2022-02-24 08:08:01 +0200

    save

--- book8/mysite/mysite/wsgi.py
+++ book8/mysite/mysite/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+import dotenv
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+ENV_FILE = Path(__file__).resolve().parent.parent / ".env"
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
