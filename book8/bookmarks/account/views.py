7ede4364e50224dcde1d1ef0ce4c883cd805475c
Madalin Popa <github@madalintech.com>
2022-03-04 06:45:21 +0200

    save

--- book8/bookmarks/account/views.py
+++ book8/bookmarks/account/views.py
@@ -9,6 +9,7 @@ from django.views.decorators.http import require_POST
 
 from .models import Profile, Contact
 from bookmarks.common.decorators import ajax_required
+from actions.utils import create_action
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 
 
@@ -56,6 +57,7 @@ def register(request):
             new_user.save()
             # create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, "has created an account")
             return render(request, "account/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
@@ -114,6 +116,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == "follow":
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, "is following", user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({"status": "ok"})
