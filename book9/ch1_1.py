7081c4d09a8f5e14273da0874c880294e0588aed
Madalin Popa <github@madalintech.com>
2022-03-11 08:10:12 +0200

    Save

---
+++ book9/ch1_1.py
@@ -0,0 +1,16 @@
+import math
+
+def check_prime(number):
+    sqrt_number = math.sqrt(number)
+    for i in range(2, int(sqrt_number) + 1):
+        if (number / i).is_integer():
+            return False
+    return True
+
+print(f"chec_prime(10,000,000) = {check_prime(10_000_000)}")
+# check_prime(10,000,000) = False
+print(f"chec_prime(10,000,000) = {check_prime(10_000_019)}")
+# check_prime(10,000,019) = True
+
+
+
